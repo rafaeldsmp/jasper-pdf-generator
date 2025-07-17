bc3e3d2c02e4911053575a31e1d794f80e2f88f3
Rafael Pino <rafaeltsd@gmail.com>
2025-07-17 12:40:00 -0300

    feat: inclusao de dependencias extras e metodos de exportar e abrir jrxml e jasper

--- src/main/java/br/com/library/JasperService.java
+++ src/main/java/br/com/library/JasperService.java
@@ -3,7 +3,10 @@ package br.com.library;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.view.JasperViewer;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -13,6 +16,43 @@ public class JasperService {
 
     public void addParams(String key, Object value) {
         this.params.put(key, value);
+    }
+
+    public void abrirJrxmlComSubReport(String arqMaster, String arqSub, Connection connection){
+        try {
+            JasperReport subReport = compilarJrxml(arqSub);
+            this.params.put("SUB_REPORT_PARAM", subReport);
+            JasperReport masterReport = compilarJrxml(arqMaster);
+            JasperPrint print = JasperFillManager.fillReport(masterReport, this.params, connection);
+            JasperViewer viewer = new JasperViewer(print);
+            viewer.setVisible(true);
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void abrirPontoJasper(String jasperFile, Connection connection){
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream(jasperFile);
+            JasperPrint print = JasperFillManager.fillReport(is, this.params, connection);
+            JasperViewer viewer = new JasperViewer(print);
+            viewer.setVisible(true);
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void exportarParaPDF(String jrxml, Connection connection, String saida){
+        JasperReport report = compilarJrxml(jrxml);
+        try {
+            OutputStream out = new FileOutputStream(saida);
+            JasperPrint print = JasperFillManager.fillReport(report, this.params, connection);
+            JasperExportManager.exportReportToPdfStream(print, out);
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void abrirJasperView(String jrxml, Connection connection){
--- src/main/java/br/com/library/Main.java
+++ src/main/java/br/com/library/Main.java
@@ -2,15 +2,56 @@ package br.com.library;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        abrirJrxml("02");
+        //abrirJrxml("18");
+//        exportarParaPdf("18", "${LOCAL_ARQUIVO}" + "jasper-" + UUID.randomUUID() + ".pdf");
+        //abrirPontoJasperComSubRelatorios("10");
+        abrirJrxmlComSubRelatorios("10");
+    }
+
+    private static void abrirPontoJasperComSubRelatorios(String numero) throws SQLException {
+        Connection connection = JdbcConnection.getConnection();
+        JasperService service = new JasperService();
+        service.abrirPontoJasper("relatorios/jasper/funcionarios-" + numero + ".jasper", connection);
+        connection.close();
+    }
+
+    private static void abrirJrxmlComSubRelatorios(String numero) throws SQLException {
+        Connection connection = JdbcConnection.getConnection();
+        JasperService service = new JasperService();
+//        service.addParams("NIVEL_DESC", "JUNIOR");
+//        service.addParams("UF", "SP");
+        service.abrirJrxmlComSubReport(
+                "relatorios/jrxml/funcionarios-" + numero + ".jrxml",
+                "relatorios/jrxml/funcionario-" + numero + "-subfone.jrxml",
+                connection);
+        connection.close();
+    }
+
+    private static void abrirPontoJasper(String numero) throws SQLException {
+        Connection connection = JdbcConnection.getConnection();
+        JasperService service = new JasperService();
+//        service.addParams("NIVEL_DESC", "JUNIOR");
+        service.addParams("UF", "SP");
+        service.abrirPontoJasper("relatorios/jasper/funcionarios-" + numero + ".jasper", connection);
+        connection.close();
+    }
+
+    private static void exportarParaPdf(String numero, String saida) throws SQLException {
+        Connection connection = JdbcConnection.getConnection();
+        JasperService service = new JasperService();
+        service.exportarParaPDF("relatorios/jrxml/funcionarios-" + numero + ".jrxml", connection, saida);
+        connection.close();
     }
 
     private static void abrirJrxml(String numero) throws SQLException {
         Connection connection = JdbcConnection.getConnection();
         JasperService service = new JasperService();
+//        service.addParams("NIVEL_DESC", "JUNIOR");
+//        service.addParams("UF", "SP");
         service.abrirJasperView("relatorios/jrxml/funcionarios-" + numero + ".jrxml", connection);
         connection.close();
     }
